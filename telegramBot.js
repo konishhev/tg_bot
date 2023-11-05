3c9faa63f2daba09841eea4378a9c43f049fb3fe
konishhev <Antonkon26@yandex.ru>
2023-11-05 21:02:51 +0700

    Update telegramBot.js

--- telegramBot.js
+++ telegramBot.js
@@ -5,12 +5,23 @@ const bot = new Bot(process.env.API_KEY_BOT);
 const doc = new GoogleSpreadsheet(process.env.GOOGLE_SERVICE_TABLE_ID,
     {apiKey: process.env.GOOGLE_SERVICE_TABLE_KEY});
 
+const dictionary = {};
+
 async function loadTable() {
     await doc.loadInfo();
     return doc.sheetsByIndex[0].getRows();
 }
 
-loadTable().then(res => console.log(res));
+loadTable().then(res => {
+    res.reduce((obj, row) => {
+        dictionary.push({
+            key: row.key,
+            request: row.request,
+            response: row.response
+        })
+    });
+    console.log(dictionary);
+});
 
 bot.command('start', async msg => {
     await msg.reply("Hey there, what's up?");
