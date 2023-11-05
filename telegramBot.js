627550a7cb217b465a5ee8ad072573a5cb315c4f
konishhev <Antonkon26@yandex.ru>
2023-11-05 20:11:48 +0700

    update

--- telegramBot.js
+++ telegramBot.js
@@ -5,9 +5,9 @@ const bot = new Bot(process.env.API_KEY_BOT);
 const doc = new GoogleSpreadsheet(process.env.GOOGLE_SERVICE_TABLE_ID,
     {apiKey: process.env.GOOGLE_SERVICE_TABLE_KEY});
 
-await doc.loadInfo();
-const dictionary = await doc.sheetsByIndex[0].getRows()
-    .then((response) => console.log(response));
+async function loadTable() {
+    await doc.loadInfo();
+}
 
 bot.command('start', async msg => {
     await msg.reply("Hey there, what's up?");
