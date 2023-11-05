a06ccac5b3ef169636dde7c918bc90af3f1cb04f
konishhev <Antonkon26@yandex.ru>
2023-11-05 19:58:58 +0700

    update

--- telegramBot.js
+++ telegramBot.js
@@ -1,22 +1,13 @@
-/*
-const telegramBot = require('node-telegram-bot-api');
-
-const bot = new telegramBot(process.env.API_KEY_BOT, {
-
-});
-
-bot.onText(/\/start/, async message => {
-    try {
-        await bot.sendMessage(message.chat.id, 'BOT STARTED');
-    } catch (error) {
-        if (error) console.log(error);
-    }
-})
- */
-
 const {Bot} = require('grammy');
+const {GoogleSpreadsheet} = require('google-spreadsheet');
 
 const bot = new Bot(process.env.API_KEY_BOT);
+const doc = new GoogleSpreadsheet(process.env.GOOGLE_SERVICE_TABLE_ID,
+    {apiKey: process.env.GOOGLE_SERVICE_TABLE_KEY});
+
+await doc.loadInfo();
+const dictionary = await doc.sheetsByIndex[0].getRows()
+    .then((response) => console.log(response));
 
 bot.command('start', async msg => {
     await msg.reply("Hey there, what's up?");
